6b83d65010bbc036a1cb063a8dc4b337ba451da3
kylepeavot <kylepeavot@oxwash.com>
2022-06-27 08:33:05 +0100

    Set up typescript, Google Typescript Style, and added URLs of the videos to get transcripts from

---
+++ index.ts
@@ -0,0 +1,11 @@
+/**
+ *
+ * Videos to get text from:
+ *
+ * https://www.youtube.com/watch?v=xbX3NOiRX_w
+ * https://www.youtube.com/watch?v=LYzGyDaeSLw
+ * https://www.youtube.com/watch?v=wldGsJTPJ1o
+ * https://www.youtube.com/watch?v=GABFYH58D-A
+ * https://www.youtube.com/watch?v=cQVg1LfmGhE
+ * https://www.youtube.com/watch?v=F4rhwjX8SSA&t=1s
+ */
